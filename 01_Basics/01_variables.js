1b1af2224e264a452eaa67d6b35acfc38aa967be
Vaibhav Kushwaha <147850697+MtVaibhav09@users.noreply.github.com>
2025-10-01 21:12:50 +0000

    01_variables

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 144553
+let accountEmail ="vaibhav@gmail.com"
+var accountPassword = "12345"
+accountCity = "Lucknow"
+
+// accountId = 2 // Not Allowed 
+
+console.log(accountId);
