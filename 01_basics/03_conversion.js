685b85386e6ca5c393627d253880265ad186af77
Vaibhav Kushwaha <147850697+MtVaibhav09@users.noreply.github.com>
2025-10-02 09:58:52 +0000

    Operations

--- 01_basics/03_conversion.js
+++ 01_basics/03_conversion.js
@@ -25,4 +25,34 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber =33 
 
 let stringNumber = String(someNumber)
-console.log(typeof stringNumber);
+//console.log(typeof stringNumber);
+
+// ***************************** Operations *****************************************
+
+let value = 3
+let negvalue = -value
+//console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = "vaibhav"
+
+let str3 = str1 + str2
+console.log(str3)
+
+console.log(+true);
+console.log (+"");
+
+let num1, num2, num3
+
+num1= num2=num3 = 2+2
+
+let gamecounter = 100
+gamecounter++;
+console.log(gamecounter);
