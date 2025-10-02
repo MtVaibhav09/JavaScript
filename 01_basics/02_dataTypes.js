0d6b701b6806ad8f2e3b1085497d052d8d19c962
Vaibhav Kushwaha <147850697+MtVaibhav09@users.noreply.github.com>
2025-10-02 08:37:44 +0000

    datatypes

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -2,3 +2,21 @@
 
 // alert(3 + 3) // we are using nodejs not the browser
 
+let name = "vaibhav"
+let age = 21
+let isLoggedIn = false 
+
+//number =>2 to power 53
+// bigint
+// string => ""
+// boolean => true/ false 
+// null => stand alone value
+// undefined =>
+// symbol => unique 
+
+
+// object
+
+console.log(typeof null);  // object
+console.log(typeof undefined); //undefined
+
