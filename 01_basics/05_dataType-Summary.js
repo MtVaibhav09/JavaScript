36a3b712c697f0c3d86faa58350cd848482a4102
Vaibhav Kushwaha <147850697+MtVaibhav09@users.noreply.github.com>
2025-10-03 09:08:38 +0000

    dataType Summary

---
+++ 01_basics/05_dataType-Summary.js
@@ -0,0 +1,35 @@
+// Premitive
+
+// 7 types : String, Number, Boolean, Null, Undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+// const outsideTemp = Null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id== anotherId);
+
+//const BigNumber = 364566618792485254865n
+
+
+//Reference (Non Premitive)
+
+// Array, Objects, Functions
+
+const heros = ["Shaktiman", "naagraj", "IronMan"]
+let myobj = {
+    name: "vaibhav",
+    age: 22,
+
+}
+
+const myFunction = function(){
+    console.log("Hello Vaibhav");
+}
+
+console.log(typeof BigNumber);
