96639651077070493261b82c9c53beba9f696c4b
Vaibhav Kushwaha <147850697+MtVaibhav09@users.noreply.github.com>
2025-10-04 12:38:04 +0000

    Arrow

--- 03_basics/03_Arrow.js
+++ 03_basics/03_Arrow.js
@@ -5,5 +5,42 @@ const user = {
 
     welcomeMessage: function (){
         console.log(`${this.username} , welcome to website`);
+        console.log(this);
     }
 }
+
+// user.welcomeMessage()
+// user.username = "Amit"
+// user.welcomeMessage()
+
+// console.log(this);
+
+// function chai (){
+//     let username = "Vaibhav"
+//     console.log (this.username);
+// }
+
+// chai()
+
+// const chai = function (){
+//     let username = "Vaibhav"
+//     console.log (this.username);
+
+// }
+
+const chai = () => {
+    let username = "Vaibhav"
+    console.log (this);
+
+}
+
+// chai()
+
+// const addTwo =(num1, num2) => {               // Basic Arrow Functions 
+//     return num1 + num2
+
+// }
+
+// const addTwo =(num1, num2) =>  num1 + num2
+const addTwo =(num1, num2) => ( num1 + num2 )
+console.log(addTwo(55, 5))
